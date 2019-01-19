6ec54ae84351a70c0b00ecce7d14b14b60360546
Jacob Dillow <jacobdillow1@gmail.com>
2019-01-18 23:54:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express();
-
+var http = require('http');
+var fs = require('fs');
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 });
 
 app.listen(process.env.PORT || 8888, () => {
