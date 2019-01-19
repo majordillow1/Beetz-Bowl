a4354f02b32c604f9e40324f0ab14c8e1ab51d02
Jacob Dillow <jacobdillow1@gmail.com>
2019-01-19 13:49:45 -0600

    added socket.io

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express();
-
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 app.get('/', (req, res) => {
   res.sendFile(__dirname +'/index.html');
 });
-
-app.listen(process.env.PORT || 8888, () => {
+app.use('/static', express.static(__dirname + '/static'));
+server.listen(process.env.PORT || 8888, () => {
   console.log('Example app listening on port 8000!')
 });
