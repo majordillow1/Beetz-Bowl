1a77851073153615143041e1d31452a8161815f7
Jacob Dillow <jacobdillow1@gmail.com>
2019-01-19 02:48:12 -0600

    adding a webpage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname +'/index.html');
 });
 
 app.listen(process.env.PORT || 8888, () => {
