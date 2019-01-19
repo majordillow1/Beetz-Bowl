0b06f0ae8323b59aca14f999ba062c7da72be482
Jacob Dillow <jacobdillow1@gmail.com>
2019-01-18 23:57:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express();
+
+
 var http = require('http');
 var fs = require('fs');
-app.get('/', (req, res) => {
+http.createServer(function (req, res) {
   fs.readFile('demofile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
-});
-
-app.listen(process.env.PORT || 8888, () => {
-  console.log('Example app listening on port 8000!')
-});
-
+}).listen(process.env.PORT || 8888);
